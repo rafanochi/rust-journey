e31c9f688d9226bf0376cbca2843dddb4d811b13
rafanochi <zawkindev@gmail.com>
2025-12-25 23:46:13 +0500

    guessing gaaaame

--- ch1/guessing_game/src/main.rs
+++ ch1/guessing_game/src/main.rs
@@ -1,3 +1,23 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    let random_num = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Guess the number: ");
+
+        let mut str = String::new();
+        io::stdin().read_line(&mut str).expect("couldn't read");
+
+        let guess: i32 = str.trim().parse().expect("error in parsing");
+
+        match guess.cmp(&random_num) {
+            Ordering::Less => println!("Lesser"),
+            Ordering::Equal => {
+                println!("YOU WOOOONN");
+                break;
+            }
+            Ordering::Greater => println!("Greater"),
+        };
+    }
 }
