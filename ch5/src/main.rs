dbc9a08ba02e65cdc29e849a729e06f8bd604966
rafanochi <zawkindev@gmail.com>
2025-12-31 00:54:47 +0500

    implement method for Person

--- ch5/src/main.rs
+++ ch5/src/main.rs
@@ -1,28 +1,38 @@
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
-
 struct Person {
     surname: String,
     name: String,
     age: u16, // some waifus are even 500+ years old
 }
-
-fn make_child(w: &Person, name: &str) -> Person {
-    Person {
-        name: name.to_string(),
-        age: 0,
-        ..(w.clone())
+impl Person {
+    fn make_child(&self, name: &str) -> Person {
+        Person {
+            name: name.to_string(),
+            age: 0,
+            ..self.clone()
+        }
     }
 }
 
+struct Undefined;
+
+struct Color(i32, i32, i32);
+
 fn main() {
     let a = Person {
-        surname: "Yuuki".to_string(),
+        surname: dbg!("Yuuki".to_string()),
         name: "Asuna".to_string(),
         age: 18,
     };
 
-    let ch = make_child(&a, "Childhiko");
+    let ch = a.make_child("Childhiko");
 
-    println!("person: {:?} \nher child: {:?}", a, ch);
+    dbg!(&a);
+
+    let black = (0, 0, 0);
+    let white = (16, 16, 16);
+    let (r, g, b) = black;
+
+    println!("person: {:#?} \nher child: {:?}", a, ch);
 }
