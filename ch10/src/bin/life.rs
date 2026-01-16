7ee9f056460cb43e98d44ae3d0d2a6e965e467d5
rafanochi <zawkindev@gmail.com>
2026-01-17 00:51:32 +0500

    lifetimes

--- ch10/src/bin/life.rs
+++ ch10/src/bin/life.rs
@@ -1,22 +1,37 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+use std::fmt::Display;
+
+static BEST_WAIFU: &str = "Reze";
+
+fn longest<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Best waifu is: {ann}");
     if x.len() > y.len() { x } else { y }
 }
 
 struct Waifu<'a> {
-    name: &'a mut str,
+    name: &'a str,
     surname: &'a str,
+}
+impl<'a> Waifu<'a> {
+    fn find_love(self, s: &str) -> &str {
+        match s.find("love") {
+            Some(i) => &s[i..i + 4],
+            _ => s,
+        }
+    }
 }
 
 fn main() {
+    let waifu: &'static str = "Hanekawa";
     let x = String::from("asuna");
     let y = String::from("senjougahara");
 
-    let mut kawori = Waifu {
-        name: "Kawori".as_mut(),
+    let kawori = Waifu {
+        name: "Kawori",
         surname: "Miyazono",
     };
 
-    kawori.name = "Kaori".as_mut();
-
-    let r = longest(&x, &y);
+    let r = longest(&x, &y, BEST_WAIFU);
 }
