e24722fad28c26979c3c713abcfb052f9d77526a
rafanochi <zawkindev@gmail.com>
2026-01-25 17:33:25 +0500

    chapter 13 done

--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -1,31 +1,39 @@
 use minigrep::search;
 use std::{env, error::Error, fs, process::exit};
 
-struct Config<'a> {
-    query: &'a str,
-    path: &'a str,
+struct Config {
+    query: String,
+    path: String,
     ignore_case: bool,
 }
-impl Config<'_> {
-    fn build(args: &[String]) -> Result<Config<'_>, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+impl Config {
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("no query"),
+        };
+
+        let path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("no query"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
-            query: &args[1],
-            path: &args[2],
-            ignore_case: ignore_case,
+            query,
+            path,
+            ignore_case,
         })
     }
 }
 
 fn run(c: &Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(c.path)?;
+    let contents = fs::read_to_string(&c.path)?;
 
-    for line in search(c.query, &contents) {
+    for line in search(&c.query, &contents) {
         println!("{line:?}");
     }
 
@@ -33,8 +41,7 @@ fn run(c: &Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Couldn't parse arguments: {err:?}");
         exit(1);
     });
