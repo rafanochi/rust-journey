03cffd5389b08629b9183c7e86a2bf560c1b61b2
rafanochi <zawkindev@gmail.com>
2026-07-11 15:11:13 +0500

    trait object: downcasting

--- src/bin/trait_object.rs
+++ src/bin/trait_object.rs
@@ -1,18 +1,39 @@
+use std::any::Any;
+
 trait Waifu {
     fn say_smth(&self);
+    fn as_any(&self) -> &dyn Any;
 }
 
 struct Reze;
+impl Reze {
+    fn last_words(&self) {
+        println!(
+            "Reze: Denji... I wonder why I didn't kill you the first time we met.\nYou know, to be honest... I've never had the chance to go to school either."
+        )
+    }
+}
 impl Waifu for Reze {
     fn say_smth(&self) {
-        println!("Hello, Denji!")
+        println!("Reze: Hello, Denji!")
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
 struct Makima;
+impl Makima {
+    fn control(&self) {
+        println!("Makima: From now on, you’re in my care... I expect 'yes' or 'woof' for answers")
+    }
+}
 impl Waifu for Makima {
     fn say_smth(&self) {
-        print!("You seem lonely Denji..")
+        println!("Makima: You seem lonely Denji..")
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
 
@@ -21,5 +42,13 @@ fn main() {
     v.push(Box::new(Reze {}));
     v.push(Box::new(Makima {}));
 
-    v.iter().for_each(|w| w.say_smth());
+    v.iter().for_each(|w| {
+        w.say_smth();
+        if let Some(wany) = w.as_any().downcast_ref::<Reze>() {
+            wany.last_words();
+        }
+        if let Some(wany) = w.as_any().downcast_ref::<Makima>() {
+            wany.control();
+        }
+    });
 }
