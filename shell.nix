0886ddbbeb15a0d88329df056901d2f06600e01a
rafanochi <zawkindev@gmail.com>
2026-07-16 11:36:53 +0500

    nixify

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs, rustToolChain, ... }:
+
+pkgs.stdenv.mkDerivation
+
+{
+  name = "rust journey";
+
+  # Compile time dependencies
+  nativeBuildInputs =
+
+    with pkgs; [
+      # Hail the Nix
+      nixd
+      statix
+      deadnix
+      alejandra
+
+      # rust
+      rustToolChain
+    ];
+
+  # Set Environment Variables
+  RUST_BACKTRACE = "full";
+  # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
