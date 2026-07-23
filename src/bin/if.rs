86d412f52650bbb480798cb076e09a1de1f2a987
rafanochi <zawkindev@gmail.com>
2026-07-23 14:17:51 +0500

    clean

--- src/bin/if.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let result: Option<i8> = Some(32);
-    if let Some(value) = result {
-        print!("Result: {}", value);
-    } else {
-        println!("There is no value");
-    }
-}
