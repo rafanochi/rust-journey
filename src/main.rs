958f0be2a776a77b5053857adbaee15bb5c59305
rafanochi <zawkindev@gmail.com>
2026-08-24 07:20:08 +0500

    change the logic to read pixels

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::{GenericImage, GenericImageView, Rgba, open};
+use image::{GenericImage, GenericImageView, Pixel, Rgba, open};
 
 static SRC: &str = "./assets/reze.png";
 static RESULT: &str = "./assets/reze.png";
@@ -6,6 +6,7 @@ static TEXT: &str = "Asuna";
 
 fn main() {
     let text: Vec<_> = TEXT.into();
+    let mut pixels_changed: Vec<(u32, u32)> = Vec::new();
 
     // Hide the text inside text
     println!("======HIDING=====");
@@ -17,6 +18,7 @@ fn main() {
         .take(text.len())
         .for_each(|(i, (x, y, Rgba([r, _g, b, a])))| {
             println!("{}", text[i].clone());
+            pixels_changed.push((x, y));
             image.put_pixel(x, y, Rgba([r, text[i], b, a]));
         });
 
@@ -25,14 +27,15 @@ fn main() {
     // Read it back
     println!("======READING=====");
     let binding = open(RESULT).unwrap();
-    let mut result = String::new();
-    binding
-        .clone()
-        .pixels()
-        .take(text.len())
-        .for_each(|(_, _, Rgba([_r, g, _b, _a]))| {
-            println!("{}", g.clone());
-            result.push(g.into());
-        });
+    let result: String = pixels_changed
+        .iter()
+        .map(|(x, y)| {
+            let pixel = binding.get_pixel(*x, *y);
+            let z = char::from(pixel[1]);
+            println!("{:?}", pixel);
+            z
+        })
+        .collect();
+
     println!("Hidden text is: {}", result)
 }
