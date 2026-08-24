71d3f1ca321a1f1798c54a7480f684d913ebdab8
rafanochi <zawkindev@gmail.com>
2026-08-24 06:51:51 +0500

    try reading the hidden text

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 use image::{GenericImage, GenericImageView, Rgba, open};
 
 static SRC: &str = "./assets/reze.png";
-// static RESULT: &str = "./assets/reze.png";
+static RESULT: &str = "./assets/reze.png";
 static TEXT: &str = "Asuna";
 
 fn main() {
     let text: Vec<_> = TEXT.into();
 
     // Hide the text inside text
+    println!("======HIDING=====");
     let mut image = open(SRC).unwrap();
     image
         .clone()
         .pixels()
         .enumerate()
         .take(text.len())
-        .for_each(|(i, (x, y, Rgba([r, g, b, a])))| {
-            image.put_pixel(x, y, Rgba([r, g + text[i], b, a]));
+        .for_each(|(i, (x, y, Rgba([r, _g, b, a])))| {
+            println!("{}", text[i].clone());
+            image.put_pixel(x, y, Rgba([r, text[i], b, a]));
         });
 
     image.save("./assets/result.png").unwrap();
 
     // Read it back
-    // let mut binding = open(RESULT).unwrap();
-    // let result: Vec<u8> = Vec::new();
-    // binding
-    //     .clone()
-    //     .pixels()
-    //     .enumerate()
-    //     .take(text.len())
-    //     .for_each(|(i, (x, y, Rgba([r, g, b, a])))| {
-    //         binding.put_pixel(x, y, Rgba([r, g + text[i], b, a]));
-    //     });
-    // binding.save("./assets/result.png").unwrap();
+    println!("======READING=====");
+    let binding = open(RESULT).unwrap();
+    let mut result = String::new();
+    binding
+        .clone()
+        .pixels()
+        .take(text.len())
+        .for_each(|(_, _, Rgba([_r, g, _b, _a]))| {
+            println!("{}", g.clone());
+            result.push(g.into());
+        });
+    println!("Hidden text is: {}", result)
 }
