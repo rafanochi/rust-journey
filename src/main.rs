e65d32ede1f593ed01ca99e162a5507a6c663d22
rafanochi <zawkindev@gmail.com>
2026-09-03 10:51:04 +0500

    so close

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::ops::BitAnd;
+
 use image::{GenericImage, GenericImageView, Pixel, Rgba, open};
 
 static SRC: &str = "./assets/reze.png";
 static RESULT: &str = "./assets/reze.png";
 static TEXT: &str = "Asuna";
+static PATTERN: &str = "!@#$";
 
 fn main() {
-    let text: Vec<_> = TEXT.into();
+    let text: Vec<_> = format!("{PATTERN}{TEXT}{PATTERN}").into();
     let mut pixels_changed: Vec<(u32, u32)> = Vec::new();
 
     // Hide the text inside text
@@ -26,16 +29,12 @@ fn main() {
 
     // Read it back
     println!("======READING=====");
+    let pattern = PATTERN.as_bytes();
     let binding = open(RESULT).unwrap();
-    let result: String = pixels_changed
-        .iter()
-        .map(|(x, y)| {
-            let pixel = binding.get_pixel(*x, *y);
-            let z = char::from(pixel[1]);
-            println!("{:?}", pixel);
-            z
-        })
-        .collect();
+    let result = binding.as_bytes().chunks(text.len()).find_map(|x| {
+        (x.starts_with(pattern) && x.ends_with(pattern))
+            .then(|| x.strip_circumfix(pattern, pattern))
+    });
 
-    println!("Hidden text is: {}", result)
+    println!("Hidden text is: {:?}", result.is_some())
 }
