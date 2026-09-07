74d66f54bd321a30fbd979e0d45e8c4972dd6832
rafanochi <zawkindev@gmail.com>
2026-09-08 00:24:53 +0500

    add little improvements and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,48 @@
 use image::{GenericImage, GenericImageView, Rgba, open};
 
 static SRC: &str = "./assets/reze.png";
-static RESULT: &str = "./assets/result.png";
+static DEST: &str = "./assets/result.png";
 static TEXT: &str = "Asuna";
 static PATTERN: &str = "!@#$";
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let text: Vec<_> = format!("{PATTERN}{TEXT}{PATTERN}").into();
 
     // Hide the text inside text
     println!("======HIDING=====");
-    let mut image = open(SRC).unwrap();
+    let mut image = open(SRC)?;
     image
         .clone()
-        .pixels()
-        .enumerate()
-        .take(text.len())
+        .pixels() // take pixels
+        .enumerate() // add indexing behaviour
+        .take(text.len()) // only take starting pixels
         .for_each(|(i, (x, y, Rgba([r, _g, b, a])))| {
+            // replace green channel with hidden text
             image.put_pixel(x, y, Rgba([r, text[i], b, a]));
         });
 
-    image.save("./assets/result.png").unwrap();
+    image.save(DEST)?;
 
     // Read it back
     println!("======READING=====");
     let pattern = PATTERN.as_bytes();
-    let binding = open(RESULT).unwrap();
-    let pixels = binding
+    let image = open(DEST)?;
+
+    // getting only green channels of pixels
+    let pixels = image
         .pixels()
         .map(|(_, _, Rgba([_r, g, _b, _a]))| g.clone())
         .collect::<Vec<u8>>();
-    let result = pixels.chunks(text.len()).find_map(|x| {
-        println!("the pixel: {:?}", String::from_utf8(x.to_vec()));
-        x.strip_circumfix(pattern, pattern)
-    });
+
+    // creating text-length chunks from pixel array, and searching for a chunk that satisfies our hidden data
+    let result = pixels
+        .chunks(text.len())
+        .find_map(|x| x.strip_circumfix(pattern, pattern));
 
     println!(
         "Hidden text is: {:?}",
-        String::from_utf8_lossy(result.unwrap())
-    )
+        String::from_utf8_lossy(result.unwrap_or_default())
+    );
+
+    Ok(())
 }
